3e60117dc9abe5d9fd14bea577cc82483ab51cd2
Eric Stern <firehed@gmail.com>
2012-12-17 01:35:39 -0800

    use newer, preferred URL helper for otpauth:// url requests

--- Authenticator/TOTPAppDelegate.m
+++ Authenticator/TOTPAppDelegate.m
@@ -16,12 +16,12 @@
     return YES;
 }
 
-- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+-(BOOL) application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
 	NSLog(@"%@", url);
 #warning this should bounce to some sort of confirmation screen
 	return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
