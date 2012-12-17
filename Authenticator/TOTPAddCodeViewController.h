d9e264e6854acaa77ea04c26ec1f87dde59118df
Eric Stern <firehed@gmail.com>
2012-12-16 16:36:59 -0800

    add outlets to the add code view controller

--- Authenticator/TOTPAddCodeViewController.h
+++ Authenticator/TOTPAddCodeViewController.h
@@ -8,9 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TOTPAddCodeViewController : UIViewController
+@interface TOTPAddCodeViewController : UIViewController <UITextFieldDelegate>
 
 -(IBAction)cancel:(id)sender;
 -(IBAction)save:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
+
+@property (weak, nonatomic) IBOutlet UITextField *accountNameField;
+@property (weak, nonatomic) IBOutlet UITextField *secretField;
+@property (weak, nonatomic) IBOutlet UITextField *stepField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *digitsControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *algorithmControl;
 
 @end
